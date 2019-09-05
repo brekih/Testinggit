7a20c9eeae76d561b078191feaaa111d8526bc9d
Breki Hardarson <brekih@gmail.com>
2019-09-05 09:46:41 +0000

    Alghorithm completed

--- max_int.py
+++ max_int.py
@@ -1,2 +1,10 @@
 #User repeatedly inputs a number until a negative value is entered,
 #print the Max input
+num_int = int(input("Input a number: "))
+max_num = num_int
+while num_int >= 0:
+    if num_int > max_num:
+        max_num = num_int
+    
+    num_int = int(input("Input a number: "))
+print("The maximum is", max_int)
